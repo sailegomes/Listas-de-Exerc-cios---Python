6b481a04e1aac3a0481ec17935f7b12bcd2ab3ad
Elias Gomes <123494424+sailegomes@users.noreply.github.com>
2023-10-13 11:14:39 -0300

    Lista1_Q1.py

---
+++ Lista_Estrutura_Sequencial/Lista1_Q1.py
@@ -0,0 +1,9 @@
+#Podemos criar uma função:
+def diga_OI():
+  print('English: Hello, world!')
+  print('Portuguese: Olá, mundo!')
+
+diga_OI()
+
+#Ou podemos simplesmente usar 'print()':
+print('Hello, world.')
