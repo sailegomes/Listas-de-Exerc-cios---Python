740915e73d4f7e72a41d7fd36aa103877e68d554
Elias Gomes <123494424+sailegomes@users.noreply.github.com>
2023-10-13 11:32:41 -0300

    Lista1_Q3.py

---
+++ Lista_Estrutura_Sequencial/Lista1_Q3.py
@@ -0,0 +1,9 @@
+def soma():
+  numero_1 = input('Diga um número:')
+  numero_1 = int(numero_1)
+  numero_2 = input('Diga um segundo número:')
+  numero_2 = int(numero_2)
+  soma = numero_1 + numero_2
+  print(f'A soma dos números é {soma}.')
+
+soma()
