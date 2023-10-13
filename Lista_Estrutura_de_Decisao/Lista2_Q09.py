97688b1188c079ae2a2fe158d9b0da93aa5918bc
Elias Gomes <123494424+sailegomes@users.noreply.github.com>
2023-10-13 11:54:26 -0300

    Create Lista2_Q09.py

---
+++ Lista_Estrutura_de_Decisao/Lista2_Q09.py
@@ -0,0 +1,12 @@
+def decresc(a,b,c):
+    lista = []
+    lista.append(a)
+    lista.append(b)
+    lista.append(c)
+    a = float(a)
+    b = float(b)
+    c = float(c)
+    d = sorted(lista)
+    print(f'Os números em forma decrescente será: {d}')
+
+decresc(5,6,2)
