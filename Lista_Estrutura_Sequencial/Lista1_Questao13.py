111c20a09a2f883507bb352424984a2e42a91889
Elias Gomes <123494424+sailegomes@users.noreply.github.com>
2023-10-13 11:45:21 -0300

    Create Lista1_Questao13.py

---
+++ Lista_Estrutura_Sequencial/Lista1_Questao13.py
@@ -0,0 +1,7 @@
+def calculadora_IMC2(altura):
+  imc_homens = (72.7 * altura) - 58
+  print(f'Para o homem, o peso ideal é de {imc_homens} Kg.')
+  imc_mulheres = (62.1 * altura) - 44.7
+  print(f'Para a mulher, o peso ideal é de {imc_mulheres} Kg.')
+
+calculadora_IMC2(1.83)
